e2d5eb3255b9d308fbd745e1bd5539a40f2fccdb
Damon Smith <damon.easy.system@gmail.com>
2016-06-12 17:58:16 +0300

    add tests of Component

---
+++ test/ComponentTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This file is part of the "Easy System" package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Damon Smith <damon.easy.system@gmail.com>
+ */
+namespace Es\Modules\Test;
+
+use Es\Modules\Component;
+
+class ComponentTest extends \PHPUnit_Framework_TestCase
+{
+    protected $requiredServices = [
+        'Modules',
+    ];
+
+    public function testGetVersion()
+    {
+        $component = new Component();
+        $version   = $component->getVersion();
+        $this->assertInternalType('string', $version);
+        $this->assertRegExp('#\d+.\d+.\d+#', $version);
+    }
+
+    public function testGetServicesConfig()
+    {
+        $component = new Component();
+        $config    = $component->getServicesConfig();
+        $this->assertInternalType('array', $config);
+        foreach ($this->requiredServices as $item) {
+            $this->assertArrayHasKey($item, $config);
+        }
+    }
+
+    public function testGetEventsConfig()
+    {
+        $component = new Component();
+        $config    = $component->getEventsConfig();
+        $this->assertInternalType('array', $config);
+    }
+}
